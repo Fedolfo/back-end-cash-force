088a0edceaf2e58abf145d3d4f03b3023ec8edb2
Filipe <filipecandido123@hotmail.com>
2022-05-07 17:30:38 -0300

    [refactor]: adiciona id no models de buyer

--- src/database/models/buyers.js
+++ src/database/models/buyers.js
@@ -1,5 +1,12 @@
 module.exports = (sequelize, DataTypes) => {
   const buyers = sequelize.define('buyers', {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+      unique: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
