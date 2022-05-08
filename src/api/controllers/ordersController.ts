0fcfa55467bbca4ecdbb42817d2f81530506f759
Filipe <filipecandido123@hotmail.com>
2022-05-08 17:53:19 -0300

    [refactor]: orderController mudança no nome do metodo getAll para getAllOrders

--- src/api/controllers/ordersController.ts
+++ src/api/controllers/ordersController.ts
@@ -1,12 +1,13 @@
 import OrdersService from '../services/ordersService';
 import { Request, Response } from 'express'
+import IOrdersController from 'api/typescript/interfaces/controllers/ordersControllers.interface';
 
-class OrdersController {
+class OrdersController implements IOrdersController {
 
   private ordersService = new OrdersService();
 
-  getAll = async (req: Request, res: Response) => {
-    const orders = await this.ordersService.getAll();
+  public getAllOrders = async (_req: Request, res: Response) => {
+    const orders = await this.ordersService.getAllOrders();
     return res.status(200).json(orders);
   }
 }
