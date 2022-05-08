35069606c4640485a7685726c217cb57d92fd678
Filipe <filipecandido123@hotmail.com>
2022-05-08 16:23:11 -0300

    [feat]: adiciona as associações de orders em OrderServices para renderizar no endpoint "/orders"

--- src/api/services/ordersService.ts
+++ src/api/services/ordersService.ts
@@ -1,11 +1,44 @@
+import Buyers from '../../database/models/buyers';
+import Cnpjs from '../../database/models/cnpjs';
+import Providers from '../../database/models/users';
 import Orders from '../../database/models/orders';
+import Users from '../../database/models/users';
 
 class OrdersService {
 
   private orders = Orders;
 
+  private buyers = Buyers;
+
+  private providers = Providers;
+
+  private cnpjs = Cnpjs;
+
+  private users = Users;
+
   async getAll() {
-    return await this.orders.findAll();
+    return await this.orders.findAll({
+      include: [
+        {
+          model: this.buyers,
+          as: 'buyer',
+        },
+        {
+          model: this.providers,
+          as: 'provider',
+          all: true,
+        },
+        {
+          model: this.cnpjs,
+          as: 'cnpj',
+        },
+        {
+          model: this.users,
+          as: 'user',
+          all: true,
+        }
+      ],
+    });
   }
 }
 
